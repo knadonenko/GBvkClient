a6a9e5a1572fd41e717782862516b76f114dde89
Konstantin <kynadonenko@gmail.com>
2020-11-25 09:41:32 +0300

    task complete

---
+++ VKClient/VKClient/CustomTransitions/CustomInteractiveTransition.swift
@@ -0,0 +1,44 @@
+//
+//  CustomInteractiveTransition.swift
+//  VKClient
+//
+//  Created by Константин Надоненко on 25.11.2020.
+//
+
+import UIKit
+
+class CustomInteractiveTransition: UIPercentDrivenInteractiveTransition {
+    
+    var viewController: UIViewController? {
+        didSet {
+            let recognizer = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleScreenGesture(_:)))
+            recognizer.edges = [.left]
+            viewController?.view.addGestureRecognizer(recognizer)
+        }
+    }
+    
+    var hasStarted: Bool = false
+    var hasEnded: Bool = false
+    
+    @objc func handleScreenGesture(_ recognizer: UIScreenEdgePanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            self.hasStarted = true
+            self.viewController?.navigationController?.popViewController(animated: true)
+        case .changed:
+            let translation = recognizer.translation(in: recognizer.view)
+            let relativeTransition = translation.x / (recognizer.view?.bounds.width ?? 1)
+            let progress = max(0, min(1, relativeTransition))
+            self.hasEnded = progress > 0.33
+            self.update(progress)
+        case .ended:
+            self.hasStarted = false
+            self.hasEnded ? self.finish() : self.cancel()
+        case .cancelled:
+            self.hasStarted = false
+            self.cancel()
+        default: return
+        }
+    }
+    
+}
