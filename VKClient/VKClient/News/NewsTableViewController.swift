c5d7e48072fb0d312bfa5415f9914df90e671a72
Konstantin <kynadonenko@gmail.com>
2021-03-08 18:56:44 +0300

    done expandable textview

--- VKClient/VKClient/News/NewsTableViewController.swift
+++ VKClient/VKClient/News/NewsTableViewController.swift
@@ -42,48 +42,12 @@ class NewsTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "NewsCell", for: indexPath) as! NewsCellTableViewCell
 
         let newsItem = newsFeed[indexPath.row]
-
-        let newsTitle = newsItem.text
         let sourceId = newsItem.source_id
-        let newsAuthor = groups.first(where: { $0.id == abs(sourceId) })?.name
+        let newsAuthor = groups.first(where: { $0.id == abs(sourceId) })?.name ?? ""
 
-        showPhoto(cell, with: newsItem)
-
-        cell.newsTitle.text = newsAuthor ?? ""
-        cell.newsText.text = newsTitle
-
-        let commentsCount = newsItem.comments?.count
-        let likeCount = newsItem.likes?.count
-        let repostCount = newsItem.reposts?.count
-        let viewsCount = newsItem.views?.count ?? 0
-        
-        if newsItem.likes?.user_likes == 1 {
-            cell.likeButton.updateLikeButton()
-        }
-
-        cell.likeButton.updateLikesCount(likes: Int(likeCount!))
-        cell.commentsCount.text = String(commentsCount!)
-        cell.repostsCount.text = String(repostCount!)
-        cell.viewsCount.text = String(viewsCount)
-
-        cell.likeButton.owner_id = newsItem.source_id
-        cell.likeButton.post_id = newsItem.post_id
+        cell.loadData(newsItem, newsAuthor)
 
         return cell
-    }
-    
-    func showPhoto(_ cell: NewsCellTableViewCell, with newsData: NewsModel) {
-        if let newsAttachments = newsData.attachments?[0] {
-            if newsAttachments.type == "photo" {
-                let imageURL = newsAttachments.photo?.sizes?.first(where: {
-                    $0.height > 300
-                })?.url
-                cell.newsImage.isHidden = false
-                cell.newsImage.sd_setImage(with: URL(string: imageURL ?? ""), placeholderImage: UIImage(named: "city"))
-            } else {
-                cell.newsImage.isHidden = true
-            }
-        }
     }
     
     func getNews() {
@@ -128,7 +92,6 @@ extension NewsTableViewController: UITableViewDataSourcePrefetching {
         }
         if maxSection > newsFeed.count - 4, !isLoading {
             isLoading = true
-            print("111111111111 LOADING")
             getNews()
             isLoading = false
         }
